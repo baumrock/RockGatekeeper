0d831ab364fd480545cb46636a2037a6c17f5133
BernhardBaumrock <office@baumrock.com>
2025-07-29 09:57:30 +0200

    feat: allow ip across different sessions

--- RockGatekeeper.module.php
+++ RockGatekeeper.module.php
@@ -9,35 +9,60 @@ namespace ProcessWire;
  */
 class RockGatekeeper extends WireData implements Module
 {
+  const cachekey = 'gatekeeper-ips';
+
   public function ready()
   {
+    $this->allowIP();
+
     // early exits for minimal footprint
     if (!wire()->config->gatekeeper) return;
     if (wire()->config->external) return; // cli usage
+    if (wire()->config->ajax) return; // ajax usage
 
-    $this->allowUser();
+    $this->checkPassword();
     $this->preventAccess();
   }
 
-  private function allowUser()
+  private function allowIP()
+  {
+    $ip = wire()->session->getIP();
+    $ips = wire()->cache->get(self::cachekey) ?: [];
+    $ips[$ip] = time() + 60 * 30; // +30min
+    wire()->cache->save(self::cachekey, $ips);
+  }
+
+  private function checkPassword()
   {
     $pass = wire()->input->get('gatekeeper');
     if (!$pass) return;
 
     // if $pass does not match, exit
     if ($pass !== wire()->config->gatekeeper) {
-      wire()->session->gatekeeperPassword = false;
-      wire()->session->gatekeeperIP = false;
+      $this->removePassword();
+      $this->removeIP();
       return;
     }
 
     // correct password, set session vars
     wire()->session->gatekeeperPassword = $pass;
-    wire()->session->gatekeeperIP = wire()->session->getIP();
+    $this->allowIP();
 
     // redirect to the same page but without the gatekeeper param
     $url = $this->removeQueryParam();
     wire()->session->redirect($url);
+  }
+
+  private function hasCorrectPassword()
+  {
+    return wire()->session->gatekeeperPassword === wire()->config->gatekeeper;
+  }
+
+  private function isAllowedIP()
+  {
+    $ips = wire()->cache->get(self::cachekey) ?: [];
+    $ip = wire()->session->getIP();
+    return isset($ips[$ip]) && $ips[$ip] > time();
   }
 
   private function preventAccess()
@@ -46,14 +71,29 @@ class RockGatekeeper extends WireData implements Module
     if (!wire()->user->isGuest()) return;
 
     // no redirect if correct password is set
-    $pass = wire()->config->gatekeeper;
-    if (wire()->session->gatekeeperPassword === $pass) return;
+    if ($this->hasCorrectPassword()) return;
 
-    // if the IP is allowed we also exit early
-    if (wire()->session->gatekeeperIP === wire()->session->getIP()) return;
+    // no password, but IP is allowed?
+    // this makes it possible to access the page from multiple devices
+    // eg when testing registration process and clicking on a link in an email
+    // on your phone instead of your desktop pc
+    if ($this->isAllowedIP()) return;
 
     // no access
     die('no access');
+  }
+
+  private function removeIP()
+  {
+    $ip = wire()->session->getIP();
+    $ips = wire()->cache->get(self::cachekey) ?: [];
+    unset($ips[$ip]);
+    wire()->cache->save(self::cachekey, $ips);
+  }
+
+  private function removePassword()
+  {
+    wire()->session->remove('gatekeeperPassword');
   }
 
   private function removeQueryParam()
